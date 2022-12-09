919277f48ab6c2a72fc8ab68655c024d61ded89d
Konstantin <kostay2013.18@gmail.com>
2022-12-09 20:30:47 +0300

    Commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,12 +1,14 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 import java.util.List;
+
 
 public class MainApp {
    public static void main(String[] args) throws SQLException {
@@ -15,19 +17,25 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User user1 = new User("Andre", "Vilson", "AV@mail.com");
+      User user2 = new User("Vlad", "Reaber", "VD@mail.com");
+      User user3 = new User("Roma", "Doolit", "RD@mail.com");
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
+      Car car1 = new Car("Top_Tachka", 777);
+      Car car2 = new Car("Norm_Tachka", 7);
+      Car car3 = new Car("Neochen_Tachka", 123);
+
+      userService.add(user1.setCar(car1).setUser(user1));
+      userService.add(user2.setCar(car2).setUser(user2));
+      userService.add(user3.setCar(car3).setUser(user3));
+
+      System.out.println("1. Adds:");
+      for (User user : userService.listUsers()) {
+         System.out.println(user + " " + user.getCar());
       }
+      System.out.println("2. Search:");
+      System.out.println(userService.getUserByCar("Norm_Tachka", 7));
+
 
       context.close();
    }
